2f25c54bfac94404a01d6230efea2434de6369f7
Christian James <james@galtrold.dk>
2014-07-22 23:56:40 +0200

    initial commit

---
+++ knockout-boot-modal.js
@@ -0,0 +1,22 @@
+﻿/**
+ * Created by Christian James 22-07-2014
+ * This code was inspired by Andrew Davey at http://aboutcode.net/2012/11/15/twitter-bootstrap-modals-and-knockoutjs.html
+ * 
+ * Requirements:
+ *  - Knockout
+ *  - knockout-amd-helpers
+ *  - requirejs
+ *  - bootstrap
+ *  - jquery
+ */
+
+define(["knockout", "jquery"],
+    function (ko, $) {
+
+        var open = function(templateId ) {
+            
+        }
+
+
+    });
+
